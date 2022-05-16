978049f72d9d823aed7eb670beb4588cf0815f4b
Pimenta <ivo.pimenta@hp.com>
2022-05-16 15:03:58 -0300

    Fetch planets, people and films

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [people, setPeople] = useState([])
+  const [planets, setPlanets] = useState([])
+  const [films, setFilms] = useState([])
+
+  // We will use this for refreshing data
+  const [loading, setLoading] = useState(true)
+
+
+  useEffect(() => {
+    async function fetchPeople() {
+      let rep = await fetch('https://swapi.dev/api/people/?format=json')
+      let data = await rep.json()
+      setPeople(data.results)
+    }
+
+    async function fetchPlanets() {
+      let rep = await fetch('https://swapi.dev/api/planets/?format=json')
+      let data = await rep.json()
+      setPlanets(data.results)
+    }
+
+    async function fetchFilms() {
+      let rep = await fetch('https://swapi.dev/api/films/?format=json')
+      let data = await rep.json()
+      setFilms(data.results)
+    }
+
+    fetchPeople()
+    fetchPlanets()
+    fetchFilms()
+  }, [])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello</p>
     </div>
   );
 }
